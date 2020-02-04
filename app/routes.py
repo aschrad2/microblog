49c7b4ac012926b87b8cb6594dded02b1af386ae
Austin Schrader <Austin.Schrader@FlandersCapital.com>
2020-02-04 15:20:15 -0800

    Updated links to dynamic generation

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,7 @@ Created on Tue Feb  4 10:09:47 2020
 
 from app import app
 from app.forms import LoginForm
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 
 @app.route('/')
 @app.route('/index')
@@ -25,11 +25,11 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
-                form.username.data, form.remember_me.data))
-        return redirect('/index')
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
