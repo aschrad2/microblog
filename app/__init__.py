ab1a443c853b1e95c00a4bbcc75d11c4b264603a
Austin Schrader <Austin.Schrader@FlandersCapital.com>
2020-02-04 13:36:18 -0800

    Added the login view function and link in navigation bar

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,9 @@ Created on Tue Feb  4 10:09:47 2020
 """
 
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,7 @@ Created on Tue Feb  4 10:09:47 2020
 
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -23,3 +24,8 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
