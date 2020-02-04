79780f1f03c9c8484ef2bbdf3945769aa79e14a1
Austin Schrader <Austin.Schrader@FlandersCapital.com>
2020-02-04 15:04:33 -0800

    Receive login credentials and redirect user to /index

--- app/routes.py
+++ app/routes.py
@@ -5,9 +5,9 @@ Created on Tue Feb  4 10:09:47 2020
 @author: austin.schrader
 """
 
-from flask import render_template
 from app import app
 from app.forms import LoginForm
+from flask import render_template, flash, redirect
 
 @app.route('/')
 @app.route('/index')
@@ -28,4 +28,8 @@ def index():
 @app.route('/login')
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+                form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
